69f173fc3b0d24d26318267de9af08d697a62157
Tina Azad <mktazad@gmail.com>
2019-10-18 15:05:09 -0400

    adding css

--- react-memory-game/src/components/Card/Card.js
+++ react-memory-game/src/components/Card/Card.js
@@ -9,7 +9,7 @@ const onClick = (props) => {
 const Card = (props) => (
     <div>
       <a href="#" className="thumbnail" onClick={() => onClick(props)}>
-        <img style={{height: 250, width: 250, marginBottom: 10}}alt = {props.name} className="image" src={props.image} />
+        <img style={{height: 150, width: 150, marginBottom: 10}}alt = {props.name} className="image" src={props.image} />
       </a>
     
  </div>

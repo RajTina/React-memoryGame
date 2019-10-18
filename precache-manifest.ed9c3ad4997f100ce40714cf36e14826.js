dd603ad29d3b5ccf5fdd1183f87f225b6af89413
Tina Azad <mktazad@gmail.com>
2019-10-18 19:39:54 -0400

    Updates

---
+++ precache-manifest.ed9c3ad4997f100ce40714cf36e14826.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea2a258c6a59d699a4f9f745ebd72fd",
+    "url": "/React-memoryGame/index.html"
+  },
+  {
+    "revision": "12ecce070b22c4abb1b4",
+    "url": "/React-memoryGame/static/css/main.f26a85ff.chunk.css"
+  },
+  {
+    "revision": "ef367c3aac24c35b0cd1",
+    "url": "/React-memoryGame/static/js/2.d676e710.chunk.js"
+  },
+  {
+    "revision": "12ecce070b22c4abb1b4",
+    "url": "/React-memoryGame/static/js/main.a0e9c2db.chunk.js"
+  },
+  {
+    "revision": "bf6ae3057b80a5615419",
+    "url": "/React-memoryGame/static/js/runtime-main.fabcb0ac.js"
+  }
+]);
